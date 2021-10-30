b4b1a0d68555d622cb69102c4dfc4ffa87235773
Laman Hasanli <hesenlileman2001@mail.ru>
2021-10-30 21:10:53 +0400

    Increase card number and change grid system.

--- js/app.js
+++ js/app.js
@@ -11,165 +11,26 @@ const flipCards = function () {
 };
 
 const renderCards = function () {
-  const numbers = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
   const shuffledNumbers = numbers.sort((a, b) => 0.5 - Math.random());
   const cardContainer = document.querySelector(".cards");
 
-  const html = `<div class="card__container">
-                  <div class="card" data-number="${shuffledNumbers[0]}">
-                    <div class="card__front">
-                      <svg class="icon--cover">
-                        <use href="images/sprite.svg#cover"></use>
-                      </svg>
-                    </div>
-                    <div class="card__back">
-                      <img class="icon--avatar" src="images/avatar${shuffledNumbers[0]}.svg" alt="" />
-                    </div>
-                  </div>
-                </div>
+  let html = ``;
 
-                <div class="card__container">
-                  <div class="card" data-number="${shuffledNumbers[1]}">
-                    <div class="card__front">
-                      <svg class="icon--cover">
-                        <use href="images/sprite.svg#cover"></use>
-                      </svg>
-                    </div>
-                    <div class="card__back">
-                      <img class="icon--avatar" src="images/avatar${shuffledNumbers[1]}.svg" alt="" />
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card__container">
-                  <div class="card" data-number="${shuffledNumbers[2]}">
-                    <div class="card__front">
-                      <svg class="icon--cover">
-                        <use href="images/sprite.svg#cover"></use>
-                      </svg>
-                    </div>
-                    <div class="card__back">
-                      <img class="icon--avatar" src="images/avatar${shuffledNumbers[2]}.svg" alt="" />
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card__container">
-                  <div class="card" data-number="${shuffledNumbers[3]}">
-                    <div class="card__front">
-                      <svg class="icon--cover">
-                        <use href="images/sprite.svg#cover"></use>
-                      </svg>
-                    </div>
-                    <div class="card__back">
-                      <img class="icon--avatar" src="images/avatar${shuffledNumbers[3]}.svg" alt="" />
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card__container">
-                  <div class="card" data-number="${shuffledNumbers[4]}">
-                    <div class="card__front">
-                      <svg class="icon--cover">
-                        <use href="images/sprite.svg#cover"></use>
-                      </svg>
-                    </div>
-                    <div class="card__back">
-                      <img class="icon--avatar" src="images/avatar${shuffledNumbers[4]}.svg" alt="" />
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card__container">
-                  <div class="card" data-number="${shuffledNumbers[5]}">
-                    <div class="card__front">
-                      <svg class="icon--cover">
-                        <use href="images/sprite.svg#cover"></use>
-                      </svg>
-                    </div>
-                    <div class="card__back">
-                      <img class="icon--avatar" src="images/avatar${shuffledNumbers[5]}.svg" alt="" />
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card__container">
-                  <div class="card" data-number="${shuffledNumbers[6]}">
-                    <div class="card__front">
-                      <svg class="icon--cover">
-                        <use href="images/sprite.svg#cover"></use>
-                      </svg>
-                    </div>
-                    <div class="card__back">
-                      <img class="icon--avatar" src="images/avatar${shuffledNumbers[6]}.svg" alt="" />
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card__container">
-                  <div class="card" data-number="${shuffledNumbers[7]}">
-                    <div class="card__front">
-                      <svg class="icon--cover">
-                        <use href="images/sprite.svg#cover"></use>
-                      </svg>
-                    </div>
-                    <div class="card__back">
-                      <img class="icon--avatar" src="images/avatar${shuffledNumbers[7]}.svg" alt="" />
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card__container">
-                  <div class="card" data-number="${shuffledNumbers[8]}">
-                    <div class="card__front">
-                      <svg class="icon--cover">
-                        <use href="images/sprite.svg#cover"></use>
-                      </svg>
-                    </div>
-                    <div class="card__back">
-                      <img class="icon--avatar" src="images/avatar${shuffledNumbers[8]}.svg" alt="" />
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card__container">
-                  <div class="card" data-number="${shuffledNumbers[9]}">
-                    <div class="card__front">
-                      <svg class="icon--cover">
-                        <use href="images/sprite.svg#cover"></use>
-                      </svg>
-                    </div>
-                    <div class="card__back">
-                      <img class="icon--avatar" src="images/avatar${shuffledNumbers[9]}.svg" alt="" />
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card__container">
-                  <div class="card" data-number="${shuffledNumbers[10]}">
-                    <div class="card__front">
-                      <svg class="icon--cover">
-                        <use href="images/sprite.svg#cover"></use>
-                      </svg>
-                    </div>
-                    <div class="card__back">
-                      <img class="icon--avatar" src="images/avatar${shuffledNumbers[10]}.svg" alt="" />
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card__container">
-                  <div class="card" data-number="${shuffledNumbers[11]}">
-                    <div class="card__front">
-                      <svg class="icon--cover">
-                        <use href="images/sprite.svg#cover"></use>
-                      </svg>
-                    </div>
-                    <div class="card__back">
-                      <img class="icon--avatar" src="images/avatar${shuffledNumbers[11]}.svg" alt="" />
-                    </div>
-                  </div>
-                </div>`;
+  for (let i = 0; i < numbers.length; i++) {
+    html += `<div class="card__container">
+            <div class="card" data-number="${shuffledNumbers[i]}">
+              <div class="card__front">
+                <svg class="icon--cover">
+                  <use href="images/sprite.svg#cover"></use>
+                </svg>
+              </div>
+              <div class="card__back">
+                <img class="icon--avatar" src="images/avatar${shuffledNumbers[i]}.svg" alt="" />
+              </div>
+            </div>
+          </div>`;
+  }
 
   cardContainer.insertAdjacentHTML("afterbegin", html);
 };
